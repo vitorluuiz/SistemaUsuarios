37bb4f578567c22726dc9842aa00637b0f802803
Vitor Luiz De Carvalho Gonçalves <vitorluizcarvalho@professor.educacao.sp.gov.br>
2025-09-18 13:23:38 -0300

    add: bcrypt

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const mysql = require("mysql2");
+
+const bcrypt = require("bcrypt");
+
 app.use(express.json());
 
 const connection = mysql.createConnection({
@@ -36,21 +39,34 @@ app.get("/usuarios", (req, res) => {
     });
 });
 
-app.post("/usuarios", (req, res) => {
-    const {nome, email, senha} = req.body;
+app.post("/usuarios", async (req, res) => {
+    const { nome, email, senha } = req.body;
 
-    const sql = "INSERT INTO Usuarios (nome, email, senha) VALUES (?, ?, ?)";
-    const values = [nome, email, senha];
+    if (nome == "" || email == "" || senha == "") {
+        res.status(400);
+        res.json({ data: "Informações inválidas" });
+    }
 
-    connection.query(sql, values, (err, results) => {
-        if (err) {
-            res.status(500).send("Erro na consulta");
-        } else {
-            res.status(201);
-            res.json(results);
-        }
-    });
+    try {
+
+        const saltRounds = 8;
+        const hash = await bcrypt.hash(senha, saltRounds);
+
+        const sql = "INSERT INTO Usuarios (nome, email, senha) VALUES (?, ?, ?)";
+        const values = [nome, email, hash];
+        
+        connection.query(sql, values, (err, results) => {
+            if (err) {
+                res.status(500).send("Erro na consulta");
+            } else {
+                res.status(201);
+                res.json(results);
+            }
+        });
+
+    } catch (err) {
+        res.status(500).json({erro: "Erro no bcrypt"});
+    }
 })
 
-
-app.listen(port, () => {console.log("aplicação rodando na porta 5000")});
+app.listen(port, () => { console.log("aplicação rodando na porta 5000") });
